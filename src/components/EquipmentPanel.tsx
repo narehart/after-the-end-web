2bfe3c85d4a969382519d337906ae8fafe947960
Nico Arehart <edmcmanwich@gmail.com>
2026-01-03 10:40:55 -0500

    feat(EquipmentPanel): add empty message when no items equipped

--- src/components/EquipmentPanel.tsx
+++ src/components/EquipmentPanel.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import { useInventoryStore, SLOT_TYPES } from '../stores/inventoryStore';
+import { FIRST_INDEX } from '../constants/numbers';
 import Panel from './Panel';
 import EquipmentSlot from './EquipmentSlot';
 import styles from './EquipmentPanel.module.css';
@@ -10,13 +11,22 @@ export default function EquipmentPanel(): React.JSX.Element {
   const equipment = useInventoryStore((state) => state.equipment);
   const equippedSlots = SLOT_TYPES.filter((slotType) => equipment[slotType] != null);
 
+  const hasEquipment = equippedSlots.length > FIRST_INDEX;
+
   return (
-    <Panel title="Equipment" border="right" className={cx('equipment-panel')}>
-      <div className={cx('equipment-slots')} role="listbox" aria-label="Equipment slots">
-        {equippedSlots.map((slotType) => (
-          <EquipmentSlot key={slotType} slotType={slotType} />
-        ))}
-      </div>
+    <Panel
+      title="Equipment"
+      border="right"
+      className={cx('equipment-panel')}
+      emptyMessage="No items equipped"
+    >
+      {hasEquipment ? (
+        <div className={cx('equipment-slots')} role="listbox" aria-label="Equipment slots">
+          {equippedSlots.map((slotType) => (
+            <EquipmentSlot key={slotType} slotType={slotType} />
+          ))}
+        </div>
+      ) : null}
     </Panel>
   );
 }
